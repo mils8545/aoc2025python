9a51102d463e94397ac8d899fa9cbb76ef9396ce
Chris Stewart <Chris.Stewart@atco.com>
2025-12-08 10:29:40 -0700

    Day 8: Quantum Baby!

--- day07/day7.py
+++ day07/day7.py
@@ -7,18 +7,50 @@ def parse_input(filename : str):
     return lines
 
 def solve_part1(lines : list[str]):
+    beams = [0] * len(lines[0])
+    
+    beams[lines[0].find("S")] = 1
 
-    return f"TODO"
+    count = 0
+    for line in lines[1:]:
+        next = [0] * len(line)
+        for i, val in enumerate(line):
+            if beams[i] > 0:
+                if val=='.':
+                    next[i] = beams[i]
+                else:
+                    count += beams[i]
+                    next[i-1] = beams[i]
+                    next[i+1] = beams[i]
+        beams = next
+
+    return f"{count}"
 
 def solve_part2(lines : list[str]):
+    beams = [0] * len(lines[0])
+    
+    beams[lines[0].find("S")] = 1
 
-    return f"TODO"
+    count = 1
+    for line in lines[1:]:
+        next = [0] * len(line)
+        for i, val in enumerate(line):
+            if beams[i] > 0:
+                if val=='.':
+                    next[i] += beams[i]
+                else:
+                    count += beams[i] 
+                    next[i-1] += beams[i]
+                    next[i+1] += beams[i]
+        beams = next
+
+    return f"{count}"
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
-        input_filename = "day05/"+sys.argv[1]
+        input_filename = "day07/"+sys.argv[1]
     else:
-        input_filename = "day05/input.txt" # Default input file name
+        input_filename = "day07/input.txt" # Default input file name
 
     puzzle_input : list[str] = parse_input(input_filename)
 
--- day08/day8.py
+++ day08/day8.py
@@ -6,77 +6,19 @@ def parse_input(filename : str):
         lines : list[str] = [line.strip() for line in f]
     return lines
 
-def neighbours(x: int, y: int, width: int, height: int) -> list[tuple[int, int]]:
-    options = [(1,0), (1,1), (1,-1), (0,1), (-1,0), (-1,-1), (-1,1), (0,-1)]
-    result : list[tuple[int,int]] = []
-    for option in options:
-        newx : int = x  + option[0]
-        newy : int = y + option[1]
-        if newx >= 0 and newx < width and newy >= 0 and newy < height:
-            result.append((newx, newy))
-    return result
-
 def solve_part1(lines : list[str]):
-    ranges : list[tuple[int,int]] = []
-    ingredients : list[int] = []
 
-    for line in lines:
-        if "-" in line:
-            left, right = line.split("-")
-            ranges.append((int(left), int(right)))
-            continue
-        if len(line) > 0:
-            ingredients.append(int(line))
-
-    count : int = 0
-
-    for ingredient in ingredients:
-        for r in ranges:
-            left, right = r
-            if ingredient >= left and ingredient <= right:
-                count += 1
-                break
-
-    return f"{count}"
+    return f"TODO"
 
 def solve_part2(lines : list[str]):
-    ranges : list[tuple[int,int]] = []
 
-    for line in lines:
-        if "-" in line:
-            left, right = line.split("-")
-            ranges.append((int(left), int(right)))
-            continue
-
-    ranges.sort()
-
-    current = 0
-    while current < len(ranges):
-        left = ranges[current][0]    
-        right = ranges[current][1]
-        i = current + 1
-        added = False
-        while i < len(ranges) and right + 1 >= ranges[i][0]:
-            right = max(ranges[i][1], right)
-            del ranges[i]
-            added = True
-        if added:
-            ranges[current] = (left, right)
-        else:
-            current += 1
-    
-    result = 0
-
-    for range in ranges:
-        result += range[1] - range[0] + 1
-
-    return f"{result}"
+    return f"TODO"
 
 if __name__ == "__main__":
     if len(sys.argv) > 1:
-        input_filename = "day05/"+sys.argv[1]
+        input_filename = "day08/"+sys.argv[1]
     else:
-        input_filename = "day05/input.txt" # Default input file name
+        input_filename = "day08/input.txt" # Default input file name
 
     puzzle_input : list[str] = parse_input(input_filename)
 
